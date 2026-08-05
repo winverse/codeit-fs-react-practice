9f2f127d7d2c8af0b49d479486ccb5dd32b6252d
winverse <public.winverse@gmail.com>
2026-08-05 11:55:16 +0900

    fix(practice): JSX 기초 시작 코드를 비운다

--- src/features/JsxBasics/Exercise.jsx
+++ src/features/JsxBasics/Exercise.jsx
@@ -1,29 +1,13 @@
-// ✋ 여기서 JSX 실습을 진행하세요!
-// 아래 주석을 보고 코드를 작성해보세요.
+// ✋ 아래 TODO를 보고 JSX를 직접 작성해보세요.
 
 function Exercise() {
   return (
-    // TODO 1: 최상위 요소가 하나인지 확인하세요
-    // TODO 2: <input /> 태그가 셀프 클로징 형태인지 확인하세요
-    // TODO 3: <ul>에 className="todo-list"를 추가하세요
-
-    // 👇 여기부터 코드를 작성하세요
     <div>
-      <h1>오늘의 할 일</h1>
-      {/* 할 일 입력 섹션 */}
-      <section>
-        <input type="text" placeholder="새로운 할 일을 입력하세요" />
-        <button>추가</button>
-      </section>
-      {/* 할 일 목록 섹션 */}
-      <section>
-        <ul>
-          <li>리액트 기초 배우기</li>
-          <li>JSX 규칙 이해하기</li>
-        </ul>
-      </section>
+      {/* TODO 1: <h1>으로 "오늘의 할 일" 제목을 작성하세요. */}
+      {/* TODO 2: <section> 안에 셀프 클로징 <input />과 "추가" 버튼을 작성하세요. */}
+      {/* TODO 3: <section> 안에 className="todo-list"인 <ul>과 두 개의 <li>를 작성하세요.
+          항목: "리액트 기초 배우기", "JSX 규칙 이해하기" */}
     </div>
-    // 👆 여기까지
   );
 }
 
