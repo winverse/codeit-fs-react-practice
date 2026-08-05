f6d1caa765a1d83d3b5b291a46014da1648e8f2d
winverse <public.winverse@gmail.com>
2026-08-05 11:58:28 +0900

    fix(practice): JSX 최상위 요소 작성을 실습하게 한다

--- src/features/JsxBasics/Exercise.jsx
+++ src/features/JsxBasics/Exercise.jsx
@@ -1,14 +1,12 @@
 // ✋ 아래 TODO를 보고 JSX를 직접 작성해보세요.
 
 function Exercise() {
-  return (
-    <div>
-      {/* TODO 1: <h1>으로 "오늘의 할 일" 제목을 작성하세요. */}
-      {/* TODO 2: <section> 안에 셀프 클로징 <input />과 "추가" 버튼을 작성하세요. */}
-      {/* TODO 3: <section> 안에 className="todo-list"인 <ul>과 두 개의 <li>를 작성하세요.
-          항목: "리액트 기초 배우기", "JSX 규칙 이해하기" */}
-    </div>
-  );
+  // TODO 1: null을 하나의 최상위 요소로 감싼 JSX로 교체하세요.
+  // TODO 2: <h1>으로 "오늘의 할 일" 제목을 작성하세요.
+  // TODO 3: <section> 안에 셀프 클로징 <input />과 "추가" 버튼을 작성하세요.
+  // TODO 4: <section> 안에 className="todo-list"인 <ul>과 두 개의 <li>를 작성하세요.
+  // 항목: "리액트 기초 배우기", "JSX 규칙 이해하기"
+  return null;
 }
 
 export default Exercise;
